7f1f6c7be8d48f0156c7211fdcca9e17d85b7805
Cellularhacker <wva3cdae@gmail.com>
2017-09-06 16:48:37 +0900

    Added core codes.

--- Aqua_Node.js/main.js
+++ Aqua_Node.js/main.js
@@ -14,3 +14,22 @@
 * 
 */
 
+
+const TelegramAPI = require('tg-cli-node');
+const config = require('./config');
+
+const Client = new TelegramAPI(config);
+
+Client.connect(connection => {
+    connection.on('message', message => {
+        console.log('message:', message);
+    });
+
+    connection.on('error', e => {
+        console.log('Error from Telegram API:', e);
+    });
+
+    connection.on('disconnect', () => {
+        console.log('Disconnected from Telegram API');
+    });
+});
