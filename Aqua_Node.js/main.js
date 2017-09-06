194bcada9cd6411328ff38ed7d0ba7d6593bbb51
Cellularhacker <wva3cdae@gmail.com>
2017-09-06 16:11:17 +0900

    Added Comments

--- Aqua_Node.js/main.js
+++ Aqua_Node.js/main.js
@@ -2,4 +2,15 @@
 * 2017-09-06 10:43 KST
 * Cellularhacker
 * Project:AVN_Aqua_bot/main.js
+* 
+* This Application is based on "TG-CLI" (which made by Lua Script).
+* So You need to install tg-cli and packages that it requires.
+* 
+***** <Linux Packages List> *****
+* libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make
+* 
+***** <Getting a tg-cli> *****
+* git clone --recursive https://github.com/vysheng/tg.git &amp;&amp; cd tg
+* 
 */
+
